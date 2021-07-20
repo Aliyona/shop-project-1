1069638c1f3f4310100e681e1a45c703fce0b1b6
Olena <zolina.alena@gmail.com>
2021-07-20 10:47:45 +0300

    added js.app and slick slider folder

---
+++ app.js
@@ -0,0 +1,177 @@
+let productsCountEl = document.getElementById("products-counter");
+let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+
+for (let i = 0; i < addToCartBtns.length; i++) {
+    addToCartBtns[i].addEventListener("click", function () {
+        //    let prevProductsCount = +productsCountEl.textContent;
+        //     productsCountEl.textContent = prevProductsCount + 1
+        productsCountEl.textContent = +productsCountEl.textContent + 1
+    })
+};
+// =====================
+// Приклади циклу for і методу forEach - для досягнення одного і того ж результату:
+// let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+// for(let i = 0; i < addToCartBtns.length; i++) {
+//     addToCartBtns[i].addEventListener("click",function() {
+//      console.log("clicked")
+//     })
+// };
+// addToCartBtns.forEach(item => {
+//     item.addEventListener("click", function () {
+//         console.log("clicked")
+//     })
+// });
+// =====================
+// modal:
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+
+moreDetailsBtns.forEach(btn => {
+    btn.addEventListener("click", openModal)
+});
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide")
+};
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show")
+};
+modal.addEventListener("click", function (e) {
+    console.log(e);
+    if (e.target === modal) {
+        closeModal()
+    }
+});
+// ================================
+// change like button state:
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+// варіант 1:
+// likeBtns.forEach((item) =>
+//     item.addEventListener("click", function () {       
+//         if (item.classList.contains("liked")) {
+//             item.classList.remove("liked")                 
+//             } else { item.classList.add("liked")}
+//                         }          
+//   )
+// );
+// варіант 2 (оптимізований):
+    likeBtns.forEach((item) =>
+        item.addEventListener("click", function () {
+            item.classList.toggle("liked")
+        })
+    );
+
+// ============================
+
+// мої спроби вирішити завдання з like:
+
+// likeBtns.forEach(btn => {
+//     btn.addEventListener("click", function() {
+//         document.getElementById("like").classList.add("like-show");
+//         document.getElementById("like").classList.remove("like")
+//     })
+// });
+
+
+// // for(let i = 0; i < likeBtns.length; i++) {
+//     likeBtns[i].addEventListener("click",function() {
+//         document.getElementById("like").classList.add("like-show");
+//         document.getElementsById("like").classList.remove("like");              
+//     })
+// likeBtns[i].addEventListener("click",function() {
+//     document.getElementById("like").classList.remove("like-show");
+//     document.getElementById("like").classList.add("like");              
+// });
+// ======================
+// 3 приклади вирішення однієї і тієї ж задачі від вчителя:
+// likeBtns.forEach((item) =>
+//   item.addEventListener("click", function (e) {
+//     e.target.style.backgroundColor = "red";
+//   })
+// );
+// або:
+// likeBtns.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     item.style.backgroundColor = "red";
+//   })
+// );
+// або:
+// likeBtns.forEach((item) =>
+//     item.addEventListener("click", function () {
+//         console.log(this);
+//     this.style.backgroundColor = "red";
+//   })
+// );
+// ============
+// scroll:
+
+// мої спроби вирішити завдання з scroll:
+// window.onscroll = function () { myFunction() };
+// function myFunction() {
+//     if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+//         modal.classList.add("show");
+//         modal.classList.remove("hide")
+//         }
+// }
+
+function showModalByScroll() {
+    // console.log(window.pageYOffset);
+    // console.log(document.body.scrollHeight);
+    if (window.pageYOffset > document.body.scrollHeight / 2) {
+        openModal();
+        window.removeEventListener("scroll", showModalByScroll);
+    }
+};
+window.addEventListener("scroll", showModalByScroll);
+
+// change product quantity:
+// ===================================
+let incrementBtns = document.querySelectorAll(".increment-button");
+let decrementBtns = document.querySelectorAll(".decrement-button");
+let quantityInputs = document.querySelectorAll(".products-quantity input");
+
+// console.log(decrementBtns);
+// console.log(incrementBtns);
+// console.log(quantityInputs);
+
+for (let i = 0; i < decrementBtns.length; i++) {
+    if (+quantityInputs[i].value > 1) {
+        decrementBtns[i].disabled = false
+    } else {
+        decrementBtns[i].disabled = true;        
+    };
+};
+for (let i = 0; i < incrementBtns.length; i++) {
+    incrementBtns[i].addEventListener("click", function () {
+        let currentValue = +quantityInputs[i].value;
+        if (currentValue < 5) {
+            quantityInputs[i].value = currentValue + 1;
+        decrementBtns[i].disabled = false;
+        } else {
+            incrementBtns[i].disabled = true;
+        }        
+    })
+};
+for (let i = 0; i < decrementBtns.length; i++) {
+    decrementBtns[i].addEventListener("click", function () {
+        let currentValue = +quantityInputs[i].value;
+        if (currentValue > 2 && currentValue <= 5) {
+            quantityInputs[i].value = currentValue - 1;
+            incrementBtns[i].disabled = false;
+        } else {
+            quantityInputs[i].value = currentValue - 1;
+            decrementBtns[i].disabled = true;            
+        }        
+    }) 
+};
+
+// =============================
+// slick slider
+
+$(".slider-block").slick({dots:true});
